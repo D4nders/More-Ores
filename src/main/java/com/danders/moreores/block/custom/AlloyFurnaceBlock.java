d4e1c14e75e24c32b62411008220fb66e6ef6011
D4nders <108522590+D4nders@users.noreply.github.com>
2024-12-19 16:09:51 +0100

    started rightclicking logic

--- src/main/java/com/danders/moreores/block/custom/AlloyFurnaceBlock.java
+++ src/main/java/com/danders/moreores/block/custom/AlloyFurnaceBlock.java
@@ -1,23 +1,40 @@
 package com.danders.moreores.block.custom;
 
+import com.danders.moreores.block.ModBlockEntityTypes;
 import com.danders.moreores.block.entity.AlloyFurnaceBlockEntity;
+import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
-public class AlloyFurnaceBlock extends Block implements EntityBlock {
+public class AlloyFurnaceBlock extends BaseEntityBlock {
 
+    public static final MapCodec<AlloyFurnaceBlock> CODEC = simpleCodec(AlloyFurnaceBlock::new);
     public static final VoxelShape SHAPE = Block.box(0, 0, 0, 16, 20, 16);
 
     public AlloyFurnaceBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    protected MapCodec<? extends BaseEntityBlock> codec() {
+        return CODEC;
     }
 
     @Override
@@ -31,7 +48,47 @@ public class AlloyFurnaceBlock extends Block implements EntityBlock {
     }
 
     @Override
+    public void onRemove(BlockState blockState, Level level, BlockPos blockPos, BlockState newBlockState, boolean isMoving) {
+        if(blockState.getBlock() != newBlockState.getBlock()) {
+            BlockEntity blockEntity = level.getBlockEntity(blockPos);
+            if(blockEntity instanceof AlloyFurnaceBlockEntity) {
+                ((AlloyFurnaceBlockEntity) blockEntity).drops();
+            }
+        }
+        super.onRemove(blockState, level, blockPos, newBlockState, isMoving);
+    }
+
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        if (!level.isClientSide && player instanceof ServerPlayer serverPlayer) {
+            MenuProvider menuProvider = getMenuProvider(state, level, pos);
+            if (menuProvider != null) {
+                serverPlayer.openMenu(menuProvider, pos);
+                return InteractionResult.CONSUME;
+            } else {
+                throw new IllegalStateException("Our MenuProvider is missing!");
+            }
+        }
+        return InteractionResult.sidedSuccess(level.isClientSide());
+    }
+
+    @Nullable
+    @Override
+    public MenuProvider getMenuProvider(BlockState state, Level level, BlockPos pos) {
+        BlockEntity blockEntity = level.getBlockEntity(pos);
+        return blockEntity instanceof AlloyFurnaceBlockEntity ? (AlloyFurnaceBlockEntity)blockEntity : null;
+    }
+
+    @Override
     public @Nullable BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return new AlloyFurnaceBlockEntity(pos, state);
+    }
+
+    @SuppressWarnings("unchecked") // Due to generics, an unchecked cast is necessary here.
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> blockEntityType) {
+        if(level.isClientSide()) {
+            return null;
+        }
+        return createTickerHelper(blockEntityType,ModBlockEntityTypes.ALLOY_FURNACE_BLOCK_ENTITY.get(), AlloyFurnaceBlockEntity::tick);
     }
 }
--- src/main/java/com/danders/moreores/block/entity/AlloyFurnaceBlockEntity.java
+++ src/main/java/com/danders/moreores/block/entity/AlloyFurnaceBlockEntity.java
@@ -4,26 +4,125 @@ import com.danders.moreores.block.ModBlockEntityTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.Containers;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.items.ItemStackHandler;
+import org.jetbrains.annotations.Nullable;
 
-public class AlloyFurnaceBlockEntity extends BlockEntity {
+public class AlloyFurnaceBlockEntity extends BlockEntity implements MenuProvider {
+    private final ItemStackHandler itemHandler = new ItemStackHandler(4);
+
+    private static final int SLOT_INPUT_1 = 0;
+    private static final int SLOT_INPUT_2 = 1;
+    private static final int SLOT_FUEL = 2;
+    private static final int SLOT_OUTPUT = 3;
+
+    protected final ContainerData data;
     private int value;
+    private static int progress = 0;
+    private static int maxProgress = 78;
 
     public AlloyFurnaceBlockEntity(BlockPos pos, BlockState blockState) {
-        super(ModBlockEntityTypes.ALLOY_BLOCK_ENTITY.get(), pos, blockState);
+        super(ModBlockEntityTypes.ALLOY_FURNACE_BLOCK_ENTITY.get(), pos, blockState);
+        this.data = new ContainerData() {
+            @Override
+            public int get(int index) {
+                return switch (index) {
+                    case 0 -> progress;
+                    case 1 -> maxProgress;
+                    default -> 0;
+                };
+            }
+
+            @Override
+            public void set(int index, int value) {
+                switch (index) {
+                    case 0 -> progress = value;
+                    case 1 -> maxProgress = value;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 4;
+            }
+        };
+    }
+
+    public static void tick(Level level, BlockPos pos, BlockState state, AlloyFurnaceBlockEntity blockEntity) {
+        if(hasRecipe()) {
+            increaseCraftingProgress();
+            setChanged(level, pos, state);
+
+            if(hasProgressFinished()) {
+                craftItem();
+                resetProgress();
+            }
+        } else {
+            resetProgress();
+        }
+    }
+
+    private static void resetProgress() {
+        progress = 0;
+    }
+
+    private static void craftItem() {
+
+    }
+
+    private static boolean hasRecipe() {
+
+    }
+
+    private static boolean hasProgressFinished() {
+        return progress >= maxProgress;
+    }
+
+    private static void increaseCraftingProgress() {
+        progress++;
     }
 
     @Override
     public void loadAdditional(CompoundTag compoundTag, HolderLookup.Provider registries) {
+        itemHandler.deserializeNBT(registries, compoundTag.getCompound("inventory"));
+        progress = compoundTag.getInt("alloy_furnace.progress");
+
         super.loadAdditional(compoundTag, registries);
-        this.value = compoundTag.getInt("value");
     }
 
     @Override
     public void saveAdditional(CompoundTag compoundTag, HolderLookup.Provider registries) {
+        compoundTag.put("inventory", itemHandler.serializeNBT(registries));
+        compoundTag.putInt("alloy_furnace.progress", progress);
+
         super.saveAdditional(compoundTag, registries);
-        compoundTag.putInt("value", this.value);
     }
 
+    public void drops() {
+        SimpleContainer inventory = new SimpleContainer(itemHandler.getSlots());
+        for(int i = 0; i < itemHandler.getSlots(); i++) {
+            inventory.setItem(i, itemHandler.getStackInSlot(i));
+        }
+        Containers.dropContents(this.level, this.worldPosition, inventory);
+    }
+
+    @Override
+    public Component getDisplayName() {
+        return Component.translatable("block.moreores.alloy_furnace");
+    }
+
+    @Override
+    public @Nullable AbstractContainerMenu createMenu(int containerId, Inventory playerInventory, Player player) {
+        return new AlloyFurnaceMenu(containerId, playerInventory, this, this.data);
+    }
 }
